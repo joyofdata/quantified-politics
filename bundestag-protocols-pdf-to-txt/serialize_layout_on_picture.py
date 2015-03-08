bc21029d60b996e0c5cec6b3b7eacccdb88e0bf9
Raffael Vogler <raffael.vogler.de@gmail.com>
2015-03-08 16:18:53 +0100

    added function for halving image

--- bundestag-protocols-pdf-to-txt/serialize_layout_on_picture.py
+++ bundestag-protocols-pdf-to-txt/serialize_layout_on_picture.py
@@ -28,6 +28,40 @@ def split_png_into_segments(pic_name, source_path, target_path):
 
 ####################################################################################
 
+def segment_vertically(img):
+    w,h = img.size 
+
+    # minimum height of three lines of standard text
+    if h < 220:
+        return []
+
+    segments = halve_image()
+    
+    if len(segments) == 0:
+        segments = quarter_image()
+
+    return segments
+
+def halve_image(img):
+    w,h = img.size
+
+    margin = w * 0.03
+    middle_area = range(int(w/2-margin), int(w/2+margin))
+    
+    non_white_columns = [(arr[:,x] < 60000).sum() for x in middle_area]
+    non_white_pixels_in_column = max(non_white_columns)
+
+    if non_white_pixels_in_column > 0.9 * h:
+        y = int(w/2-margin) + non_white_columns.index(non_white_pixels_in_column)
+        return [(0, y-10), (y+10, w-1)]
+    else:
+        return []
+
+def quarter_image(img):
+    w,h = img.size
+
+####################################################################################
+
 def segment_horizontally(img, min_break_height=100, clr_threshold=60000, ratio_threshold=0.995, padding=10):
     w,h = img.size
 
@@ -66,7 +100,7 @@ def segment_horizontally(img, min_break_height=100, clr_threshold=60000, ratio_t
 
     y_content = list(zip(y_breaks[0::2], y_breaks[1::2]))
 
-    print(y_content)
+    #print(y_content)
     
     return y_content
 
