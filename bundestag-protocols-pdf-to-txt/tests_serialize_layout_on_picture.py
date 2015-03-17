a7ddea7057eb63140c13e852a43944aab56d3915
Raffael Vogler <raffael.vogler.de@gmail.com>
2015-03-17 13:47:04 +0100

    more tests added

--- bundestag-protocols-pdf-to-txt/tests_serialize_layout_on_picture.py
+++ bundestag-protocols-pdf-to-txt/tests_serialize_layout_on_picture.py
@@ -36,17 +36,63 @@ res = segment_vertically(img,
 print(len(res)==0)
 print(res)
 
+#########################################################################
+#
+# configuration for horizontal segmentation:
+
+min_break_height=35
+ratio_threshold_horizontally=0.995
 
 # 3
 # expected are 9 segments
-
-min_break_height=35
 
 img = Image.open("tests_imgs/18003.pdf-159_p01_p01.png")
 wht = white_value(img)
 res = segment_horizontally(img,
         min_break_height=min_break_height,
-        color_threshold=int(wht*0.9))
+        color_threshold=int(wht*0.9),
+        ratio_threshold=ratio_threshold_horizontally)
 
 print(len(res)==9, " / ", len(res))
 print(res)
+
+# 4
+# expected is 1 segment
+
+img = Image.open("tests_imgs/18003.pdf-100_p01_p01.png")
+wht = white_value(img)
+res = segment_horizontally(img,
+        min_break_height=min_break_height,
+        color_threshold=int(wht*0.9),
+        ratio_threshold=ratio_threshold_horizontally)
+
+print(len(res)==0, " / ", len(res))
+print(res)
+
+
+# 5
+# expected are 2 segments
+
+img = Image.open("tests_imgs/18003.pdf-159_p01_p02_p07.png")
+wht = white_value(img)
+res = segment_horizontally(img,
+        min_break_height=min_break_height,
+        color_threshold=int(wht*0.9),
+        ratio_threshold=ratio_threshold_horizontally)
+
+print(len(res)==2, " / ", len(res))
+print(res)
+
+# 6
+# expected are 9 segments
+
+img = Image.open("tests_imgs/18003.pdf-022_p01_p01.png")
+wht = white_value(img)
+res = segment_horizontally(img,
+        min_break_height=min_break_height,
+        color_threshold=int(wht*0.9),
+        ratio_threshold=ratio_threshold_horizontally)
+
+print(len(res)==9, " / ", len(res))
+print(res)
+
